c3973083e8f6484ca85bea018ad88e1aa3ceb904
tedwards2018 <tedwards2018@student.amsacs.org>
2016-05-13 14:24:56 -0400

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -21,7 +21,7 @@ public class Maze extends JComponent{
 		int num = rand.nextInt(6);
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setSize(750,750);
+		frame.setSize(1000,1000);
 		frame.setVisible(true);
 		Graphics g = frame.getGraphics();
 		BufferedImage img = null;
@@ -43,9 +43,23 @@ public class Maze extends JComponent{
 		else{
 			img = ImageIO.read(new File("src\\maze6.png"));
 		}
-		g.drawImage(img, 0, 0, 750, 750, null);
+		g.drawImage(img, 10, 30, 800, 800, null);
 		if(img.getRGB(1, 1)!=0)
 			System.out.println("wall");
+		int r=0,c=0;
+		Color myBlue = new Color(63,72,204);
+		for(int row=0;row<img.getWidth();row++){
+			for(int col=0;col<img.getHeight();col++){
+				if(img.getRGB(row, col)==myBlue.getRGB()){
+					r=row;
+					c=col;
+					g.fillOval(r+10,c+10,40,40);
+					System.out.println("start found");
+					row=img.getWidth()*10;
+					col=img.getHeight()*10;
+				}
+			}
+		}
 	}
 	public int getMazeSize(){
 		return frame.getHeight();
