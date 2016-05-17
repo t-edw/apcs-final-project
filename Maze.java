f5d3750ee80fa02c48ed433a6f2f996ca1ecee5a
tedwards2018 <tedwards2018@student.amsacs.org>
2016-05-17 12:03:35 -0400

    Add files via upload

--- Maze.java
+++ Maze.java
@@ -12,48 +12,74 @@ import java.awt.image.*;
 import javax.imageio.*;
 import javax.swing.*;
 import java.util.*;
+import javax.swing.Action;
 
-public class Maze extends JComponent{
+public class Maze extends JFrame{
 	private JFrame frame;
 	private JPanel pane;
+	private BufferedImage img = null;
+	private Graphics g;
 	public Maze() throws IOException{
 		Random rand = new Random();
-		int num = rand.nextInt(6);
+		//int num = rand.nextInt(6);
+		int num=3;
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		frame.setSize(1000,1000);
+		frame.setSize(950,950);
 		frame.setVisible(true);
-		Graphics g = frame.getGraphics();
-		BufferedImage img = null;
+		g = frame.getGraphics();
+		int width=0;
+		int offset=0;
 		if(num==0){
 			img = ImageIO.read(new File("src\\maze1.png"));
+			width=32;
+			offset=5;
 		}
 		else if(num==1){
 			img = ImageIO.read(new File("src\\maze2.png"));
+			width=30;
+			offset=10;
 		}
 		else if(num==2){
 			img = ImageIO.read(new File("src\\maze3.png"));
+			width=25;
+			offset=10;
 		}
 		else if(num==3){
 			img = ImageIO.read(new File("src\\maze4.png"));
+			width=30;
+			offset=15;
 		}
 		else if(num==4){
+			//WHY
 			img = ImageIO.read(new File("src\\maze5.png"));
+			width=10;
 		}
 		else{
 			img = ImageIO.read(new File("src\\maze6.png"));
+			width=56;
+			offset=-35;
 		}
-		g.drawImage(img, 10, 30, 800, 800, null);
-		if(img.getRGB(1, 1)!=0)
-			System.out.println("wall");
+		g.drawImage(img, 10, 30, 900, 900, null);
+//		if(img.getRGB(1, 1)!=0)
+//			System.out.println("wall");
+		start(offset,width);
+		
+	}
+	public void start(int offset, int width){
 		int r=0,c=0;
+		int size;
 		Color myBlue = new Color(63,72,204);
 		for(int row=0;row<img.getWidth();row++){
 			for(int col=0;col<img.getHeight();col++){
 				if(img.getRGB(row, col)==myBlue.getRGB()){
 					r=row;
 					c=col;
-					g.fillOval(r+10,c+10,40,40);
+//					while(img.getRGB(row+38+offset,col+38+offset)!=Color.WHITE.getRGB()){
+//						offset++;
+//					}
+					g.setColor(Color.GREEN);
+					g.fillOval(r+width+offset,c+offset+width,width/2,width/2);
 					System.out.println("start found");
 					row=img.getWidth()*10;
 					col=img.getHeight()*10;
