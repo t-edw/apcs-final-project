fdc4e516cd5e49a73fcd1fe242bed335127b003d
tedwards2018 <tedwards2018@student.amsacs.org>
2016-05-31 22:03:33 -0400

    Update Pixel.java

--- Pixel.java
+++ Pixel.java
@@ -1,15 +1,25 @@
-
+/**Pixel.java
+ * Joshua Sathyaraj and Tamsin Edwards
+ * Period G
+ * June 1, 2016
+ * 
+ * The Pixel object used in Maze.java
+ */
 public class Pixel {
 	private int x, y; private boolean isBlack;
 	
+	//contructor
+	//creates a pixel object
 	public Pixel(int x, int y, boolean isBlack) {
 		this.x=x; this.y=y; this.isBlack=isBlack;
 	}
-	
+	//method: void setBlack(boolean isBlack)
+	//changes the value of isBlack for the pixel object
 	public void setBlack(boolean isBlack) {
 		this.isBlack=isBlack;
 	}
-	
+	//method: boolean isBlack()
+	//returns the value of isBlack for the current pixel
 	public boolean isBlack() {
 		return isBlack;
 	}
